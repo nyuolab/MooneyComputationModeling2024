b2dd8e0513f3faa06427cf0f3b33c01a095cbcc8
Xujin Chris Liu <liuchris34@gmail.com>
2024-06-15 10:04:54 -0700

    add license

--- workflow/scripts/visualization/plot_learning_long.py
+++ workflow/scripts/visualization/plot_learning_long.py
@@ -1,3 +1,19 @@
+# This file is part of Mooney computational modeling project.
+#
+# Mooney computational modeling project is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mooney computational modeling project is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mooney computational modeling project. If not, see <https://www.gnu.org/licenses/>.
+
+
 import wandb
 import seaborn as sns
 import pandas as pd
